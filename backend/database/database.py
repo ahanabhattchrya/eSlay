90ebc4dff287694da456c752959264e173d2a9bf
rlramhap <rlramhap@buffalo.edu>
2022-10-31 16:50:59 -0400

    added database collections feat charles

--- backend/database/database.py
+++ backend/database/database.py
@@ -8,6 +8,19 @@ import os
 import sys
 
 # Initialize the Mongo connection here
-mongo_client = MongoClient('mongo')
-
+mongoClient = MongoClient('mongo')
+db = mongoClient["eSlay"]
 # Initialize collections here
+
+userAccts = db["userAccts"] #collection #1: user accounts
+itemListings = db["itemListings"] # collection #2: item listings
+
+
+def insert_data():
+    '''insert data to collections userAccts and itemListings'''
+
+def delete_data():
+    '''remove data from collections userAccts and itemListings'''
+
+def update_data():
+    '''update data in userAccts and itemListings'''
