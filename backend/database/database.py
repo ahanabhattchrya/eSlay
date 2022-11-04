e9b7a91063dc2c8ced67026c092d86cfb0fa1bca
Leotrim Mehmetaj <mleotrim@gmail.com>
2022-11-04 19:52:05 -0400

    Added comments describing the format of the data. Possible discussion for adding a layer of abstraction

--- backend/database/database.py
+++ backend/database/database.py
@@ -22,11 +22,35 @@ itemListings = db["itemListings"] # collection #2: item listings
     there will be two formats that will be followed. The first one will
     pertain to the collection userAccts and the second one will pertain to
     the collection itemListings.
+
+    userAccts Format:
+
+    {
+        "username" : username, 
+        "password" : password, 
+        "clientId" : id from client address,
+        "totalMade" : total $ made,
+        "currBid" : the current bid tbis user holds,
+        "cartList" : list of item objects in this users cart,
+        "itemsForSale" : items this user put up for sale,
+        "itemsPurchased" : items this user has purchased,
+        "pointsObtained" : # of points this user has obtained
+    }
+
+    
+    itemListings Format:
+
+    WORK-IN-PROGRESS (COULD BE USING AN ITEM CLASS)
+
 '''
 
-def insert_data():
+# Parameters : 
+#   collection - collection to input to
+#   data - data that needs to be inserted
+def insert_data(data, collection):
     '''insert data to collections userAccts and itemListings'''
 
+    
 def delete_data():
     '''remove data from collections userAccts and itemListings'''
 
