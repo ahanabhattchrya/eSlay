59b36e7b6dc841b50f81742c22da4b5d3cdafccd
srcalo <smilingcalo@gmail.com>
2022-11-14 17:05:19 -0500

    Added button and login link

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -1,6 +1,6 @@
 function Registration() {
     return(
-        <div className="Registration">
+        <section className="Registration">
             <div className="form-section">
                 <form>
                     <label for='email'>Email: </label>
@@ -11,9 +11,13 @@ function Registration() {
                     <input type='text' id='password' name='password'/>
                     <label for='password2'>Confirm Password: </label>
                     <input type='text' id='password2' name='email'/>
+                    <div className="button-section">
+                        <button className="button" type='submit'>Register</button>
+                        <p>Already have an account? <a href='/login'>Log in here</a></p>
+                    </div>
                 </form>
             </div>
-        </div>
+        </section>
     )
 }
 
