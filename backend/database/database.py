f258d6df9f2f64837b416ef0f4a3b39cff6f3e35
Leotrim Mehmetaj <mleotrim@gmail.com>
2022-11-18 14:49:12 -0500

    Adding the insert function back

--- backend/database/database.py
+++ backend/database/database.py
@@ -4,6 +4,7 @@
 
 # Imports
 from pymongo import MongoClient
+from user import User
 import os
 import sys
 
@@ -16,8 +17,36 @@ userAccts = db["userAccts"] #collection #1: user accounts
 itemListings = db["itemListings"] # collection #2: item listings
 
 
-def insert_data():
+def insert_data(data, collection):
     '''insert data to collections userAccts and itemListings'''
+
+    if collection == 1:
+
+        all_users = userAccts.find({})
+        if data["username"] in all_users:
+            return "error"
+
+        new_user = {}
+        new_user["username"] = data["username"]
+
+        # Salt and hash password here
+        password = "PLACEHOLDER"
+
+        new_user_object = User(
+            data["username"],
+            password, 
+            data["clientId"],
+            data["totalMade"],
+            data["currBid"],
+            data["cartList"],
+            data["itemsForSale"],
+            data["itemsPurchased"],
+            data["pointsObtained"]
+        )
+
+        userAccts.insert_one(new_user)
+    else:
+        pass
 
 def delete_data():
     '''remove data from collections userAccts and itemListings'''
