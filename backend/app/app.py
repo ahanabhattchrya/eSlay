7313d9d754fcf051874c5305d4bb10263e3e933b
Timothy Kong <techtim@Timothys-MacBook-Pro-Max.local>
2022-11-20 00:21:22 -0500

    added registration route

--- backend/app/app.py
+++ backend/app/app.py
@@ -14,6 +14,16 @@ def frontendjs():
     pass
     # return app.send_static_file("frontend.js")
 
+
+
+
+
+
+@app.route('/registration')
+def registration(): 
+    pass
+    # return app.send_static_file()
+
 #for the get and post request
 
 if __name__ == '__main__':
