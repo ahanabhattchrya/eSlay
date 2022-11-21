743ae2ef32e324513a910d6bbf955270e5ebfa59
ahanabhattchrya <ahanabha@buffalo.edu>
2022-11-20 23:05:42 -0500

    Added all material ui components for login

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,17 +1,17 @@
 /* This page is the login page for users to login to their respective accounts*/
 import React from 'react';
 import InputMask from 'react-input-mask';
-import { TextField, Button, Box, Select, MenuItem, InputLabel, Checkbox } from '@material-ui/core';
-
+import { TextField, Button, Select, MenuItem, InputLabel, Checkbox } from '@material-ui/core';
+import Box from '@mui/material/Box';
 
 const Login = () => {
-	return (
-		<div>
-			<p>Login Page</p>
+    return (
+        <Box sx={{ width: 500, height: 500, border: '3px solid black', borderRadius: 2}}>
             <TextField required id="outlined-required" label="Username" variant="outlined" />
             <TextField required id="outlined-required" label="Password" variant="outlined" />
-		</div>
-	)
+            <Button variant="contained">Login Here</Button>
+        </Box>
+      );
 };
 
 export default Login;
