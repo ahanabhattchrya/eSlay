0644d7001a5ca9ec8af850cbdce37f46ef621bc5
ahanabhattchrya <ahanabha@buffalo.edu>
2022-11-20 23:42:48 -0500

    Centered Items

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -3,16 +3,19 @@ import React from 'react';
 import InputMask from 'react-input-mask';
 import { TextField, Button, Select, MenuItem, InputLabel, Checkbox } from '@material-ui/core';
 import Box from '@mui/material/Box';
+import './css/App.scss';
 
 const Login = () => {
     return (
-        <Box sx={{ width: 500, height: 500, border: '3px solid black', borderRadius: 2}}>
-            <TextField required id="outlined-required" label="Username" variant="outlined" />
-            <TextField required id="outlined-required" label="Password" variant="outlined" />
-            <Button variant="contained">Login Here</Button>
-            <h6>New to E-Slay? Sign Up Here</h6>
-            <h6>Forgot Password?</h6>
-        </Box>
+        <div className = "login">
+            <Box className= "login-box" sx={{ width: 500, height: 500, border: '3px solid black', borderRadius: 2, display: 'flex', alignItems: 'center'}}>
+                <TextField required id="outlined-required" label="Username" variant="outlined" />
+                <TextField required id="outlined-required" label="Password" variant="outlined" />
+                <Button variant="contained">Login Here</Button>
+                <h6>New to E-Slay? Sign Up Here</h6>
+                <h6>Forgot Password?</h6>
+            </Box>
+        </div>
       );
 };
 
