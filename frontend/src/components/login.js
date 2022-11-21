3402008bdea59e66966f085624dbca5af21d500b
ahanabhattchrya <ahanabha@buffalo.edu>
2022-11-20 22:46:25 -0500

    Added input placeholders for username and password

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,8 +1,15 @@
 /* This page is the login page for users to login to their respective accounts*/
+import React from 'react';
+import InputMask from 'react-input-mask';
+import { TextField, Button, Box, Select, MenuItem, InputLabel, Checkbox } from '@material-ui/core';
+
+
 const Login = () => {
 	return (
 		<div>
 			<p>Login Page</p>
+            <TextField required id="outlined-required" label="Username" variant="outlined" />
+            <TextField required id="outlined-required" label="Password" variant="outlined" />
 		</div>
 	)
 };
