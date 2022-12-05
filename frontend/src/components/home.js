5b5801f0aae1304e0e11211eac96c286929c141b
ahanabhattchrya <ahanabha@buffalo.edu>
2022-12-05 18:56:28 -0500

    fixed main page components

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,7 +2,7 @@
     We will then call this component in <App /> along with all the other paths*/ 
 import React from 'react';
 import InputMask from 'react-input-mask';
-import { TextField, Button, Select, MenuItem, InputLabel, Checkbox } from '@material-ui/core';
+import { TextField, Button, Select, MenuItem, InputLabel, Checkbox, Typography } from '@material-ui/core';
 import { spacing } from '@mui/system';
 import Box from '@mui/material/Box';
 import './css/App.scss';
@@ -10,14 +10,18 @@ import './css/App.scss';
 const Home = () => {
     return (
         <div className = "home">
-            <Box className= "home-box1" sx={{ width: 500, height: 500, border: '3px solid black', borderRadius: 2, display: 'flex', alignItems: 'center', mb: 5}}>
-                <h2> Why e-slay?</h2>
-                <br/>
-                <h6> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</h6>
+            <Box className= "home-box1" sx={{ width: 650, height: 400, border: '3px solid black', borderRadius: 2, display: 'flex', alignItems: 'center', mb: 5, mt: 7, ml: 10, lineHeight: 4}}>
+                <Typography component="div">
+                    <Box sx={{ lineHeight: 2, ml: 22, fontSize: "h3.fontSize", fontWeight: 'bold', mt:-5}}>Why E-Slay?</Box>
+                    <Box sx={{ lineHeight: 2, ml: 5, mr: 5, fontSize: "h5.fontSize", textAlign: 'center'}}>Because it's slay and better than ebay! We provide live auctions and various items from other users all within the reach of your fingertips! Keep scrolling to know more about us and what we do!</Box>
+                </Typography>
             </Box>
-            <Box className= "home-box2" sx={{ width: 500, height: 500, border: '3px solid black', borderRadius: 2, display: 'flex', alignItems: 'center'}}>
-                <h2>Ready to Sign Up?</h2>
-                <h3>There's a whole new world of people ready to buy your used or new items. Register here now!</h3>
+            <Box className= "home-box2" sx={{ width: 650, height: 450, border: '3px solid black', borderRadius: 2, display: 'flex', alignItems: 'center', ml: 116, mt: -30}}>
+                <Typography component="div">
+                    <Box sx={{ lineHeight: 2, ml: 18, mt:-5, fontSize: "h3.fontSize", fontWeight: 'bold'}}>First Time Here?</Box>
+                    <Box sx={{ lineHeight: 2, ml: 5, mr: 5, fontSize: "h5.fontSize", textAlign: 'center'}}>There's a whole world of people ready to buy your used or new items. <br/> Register here now!</Box>
+                    
+                </Typography>
             </Box>
         </div>
       );
