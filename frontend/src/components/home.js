05c58e9626c42e297d2ab1b7229926846c987ae9
srcalo <smilingcalo@gmail.com>
2022-12-05 19:52:43 -0500

    Updated imports

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,34 +1,36 @@
 /*This page is the / path, meaning everything that should rendered when the user visits our site should be here. 
-    We will then call this component in <App /> along with all the other paths*/ 
-import React from 'react';
-import InputMask from 'react-input-mask';
-import { TextField, Button, Select, MenuItem, InputLabel, Checkbox, Typography } from '@material-ui/core';
-import { spacing } from '@mui/system';
+    We will then call this component in <App /> along with all the other paths*/
+import React from "react";
+import { Button, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import Box from '@mui/material/Box';
-import '../assets/css/home.scss'
-
+import Box from "@mui/material/Box";
+import "../assets/css/home.scss";
 
 const Home = () => {
-    return (
-        <div className = "home">
-            <Box className= "home-box home-box1" sx={{mb: 5, mt: 7, ml: 20, lineHeight: 4}}>
-                <Typography component="div">
-                    <Box sx={{ lineHeight: 2, ml: 22, fontSize: "h3.fontSize", fontWeight: 'bold', mt:5}}>Why E-Slay?</Box>
-                    <Box sx={{ lineHeight: 2, ml: 5, mr: 5, fontSize: "h5.fontSize", textAlign: 'center'}}>Because it's slay and better than ebay! We provide live auctions and various items from other users all within the reach of your fingertips! Keep scrolling to know more about us and what we do!</Box>
-                </Typography>
-            </Box>
-            <Box className= "home-box home-box2" sx={{ml: 108, mt: -10}}>
-                <Typography component="div">
-                    <Box sx={{ lineHeight: 2, ml: 18, mt:5, fontSize: "h3.fontSize", fontWeight: 'bold'}}>First Time Here?</Box>
-                    <Box sx={{ lineHeight: 2, ml: 5, mr: 5, fontSize: "h5.fontSize", textAlign: 'center'}}>There's a whole world of people ready to buy your used or new items. <br/> <b>Register here now!</b></Box>
-                </Typography>
-                <Button variant="contained" color="primary" component={Link} to="/register">Register</Button>
-            </Box>
-        </div>
-      );
+	return (
+		<div className="home">
+			<Box className="home-box home-box1" sx={{ mb: 5, mt: 7, ml: 20, lineHeight: 4 }}>
+				<Typography component="div">
+					<Box sx={{ lineHeight: 2, ml: 22, fontSize: "h3.fontSize", fontWeight: "bold", mt: 5 }}>Why E-Slay?</Box>
+					<Box sx={{ lineHeight: 2, ml: 5, mr: 5, fontSize: "h5.fontSize", textAlign: "center" }}>
+						Because it's slay and better than ebay! We provide live auctions and various items from other users all within the reach of your fingertips! Keep scrolling to know more about
+						us and what we do!
+					</Box>
+				</Typography>
+			</Box>
+			<Box className="home-box home-box2" sx={{ ml: 108, mt: -10 }}>
+				<Typography component="div">
+					<Box sx={{ lineHeight: 2, ml: 18, mt: 5, fontSize: "h3.fontSize", fontWeight: "bold" }}>First Time Here?</Box>
+					<Box sx={{ lineHeight: 2, ml: 5, mr: 5, fontSize: "h5.fontSize", textAlign: "center" }}>
+						There's a whole world of people ready to buy your used or new items. <br /> <b>Register here now!</b>
+					</Box>
+				</Typography>
+				<Button variant="contained" color="primary" component={Link} to="/register">
+					Register
+				</Button>
+			</Box>
+		</div>
+	);
 };
 
 export default Home;
-
-
