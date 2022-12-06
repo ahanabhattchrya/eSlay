cbb06159e3f241cdf990aa4196d06689cfb21e68
srcalo <smilingcalo@gmail.com>
2022-12-06 17:46:09 -0500

    Import restructure and dashboard base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { globalTheme } from "./assets/globalTheme.js";
 
+import Navbar from "./components/navbar.js";
+import Home from "./components/home.js";
 import Register from "./components/register.js";
 import Login from "./components/login.js";
-import Home from "./components/home.js";
-import Navbar from "./components/navbar.js";
+import Dashboard from "./components/dashboard.js";
+
 import "./assets/css/eslay.scss";
 
 function App() {
@@ -17,9 +19,10 @@ function App() {
 					<Router>
 						<Navbar />
 						<Routes>
+							<Route exact path="/" element={<Home />} />
 							<Route exact path="/register" element={<Register />} />
 							<Route exact path="/login" element={<Login />} />
-							<Route exact path="/" element={<Home />} />
+							<Route exact path="/dashboard" element={<Dashboard />} />
 						</Routes>
 					</Router>
 				</div>
---
+++ frontend/src/components/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Button, Grid } from "@material-ui/core";
+
+const Dashboard = () => {
+	return (
+		<div class="dashboard">
+			<Grid></Grid>
+		</div>
+	);
+};
+
+export default Dashboard;
