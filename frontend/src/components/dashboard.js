e260b09e8f607aaef57364d2cd5195f4e48e8107
srcalo <smilingcalo@gmail.com>
2022-12-06 17:52:52 -0500

    Wrote upper-level structure

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -3,10 +3,35 @@ import { Box, Button, Grid } from "@material-ui/core";
 
 const Dashboard = () => {
 	return (
-		<div class="dashboard">
-			<Grid></Grid>
+		<div className="dashboard">
+			<Grid>
+				<Grid item>
+					<UserInfo />
+				</Grid>
+				<Grid item>
+					<Grid className="user-lists" direction="column">
+						<Grid item>
+							<Listings />
+						</Grid>
+						<Grid item>
+							<History />
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
 
+const UserInfo = () => {
+	return <div className="user-info"></div>;
+};
+
+const Listings = () => {
+	return <div className="user-items"></div>;
+};
+
+const History = () => {
+	return <div className="purchase-history"></div>;
+};
 export default Dashboard;
