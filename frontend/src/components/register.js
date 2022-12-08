e68457599f48db9066dd511c9d38257ce434c6eb
Leotrim <mleotrim@gmail.com>
2022-12-07 23:57:16 -0500

    Adding functionality for sending data to backend

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -3,15 +3,46 @@ import React from "react";
 import { TextField, Button } from "@material-ui/core";
 import Box from "@mui/material/Box";
 
+// This will handle all our registration information to send to the backend.
+let registerInfo = {
+	email : "",
+	username: "",
+	password: "",
+	confirmedPassword: ""
+};
+
+// This handler will verify passwords are the same, throw error if not
+// and then will send our data to the backend through /register
+function sendRegisterInfo(){
+	verifyPasswords(registerInfo.password, registerInfo.confirmedPassword);
+};
+
+
+/*
+ * All functions for the onChange events will happen here. 
+ * This is to help with updating the registerInfo dict
+ * with updated values
+ */
+
+function changeEmail(value) { registerInfo.email = value; };
+
+function changeUsername(value) { registerInfo.username = value; };
+
+function changePassword(value) { registerInfo.password = value; };
+
+function changeConfirmedPassword(value) { registerInfo.confirmedPassword = value; };
+
+
+
 const Register = () => {
 	return (
 		<div className="register form-container">
 			<Box className=" form-box register-box">
-				<TextField required id="outlined-required" label="Email" variant="outlined" />
-				<TextField required id="outlined-required" label="Username" variant="outlined" />
-				<TextField required id="outlined-required" label="Password" variant="outlined" />
-				<TextField required id="outlined-required" label="Confirm password" variant="outlined" />
-				<Button variant="contained" color="primary" path="/register" size="large">
+				<TextField required id="outlined-required" label="Email" variant="outlined" onChange={event => changeEmail(event.target.value)} />
+				<TextField required id="outlined-required" label="Username" variant="outlined" onChange={event => changeUsername(event.target.value)} />
+				<TextField required id="outlined-required" label="Password" variant="outlined" onChange={event => changePassword(event.target.value)} />
+				<TextField required id="outlined-required" label="Confirm password" variant="outlined" onChange={event => changeConfirmedPassword(event.target.value)} />
+				<Button variant="contained" color="primary" onClick={sendRegisterInfo} size="large">
 					Register
 				</Button>
 				<div>
