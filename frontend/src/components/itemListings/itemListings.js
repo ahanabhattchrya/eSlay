baeafb47cc456669da4eaefa829f2c6a3f77595f
Leotrim <mleotrim@gmail.com>
2022-12-08 20:31:59 -0500

    Adding all files needed for item listings

--- frontend/src/components/itemListings/itemListings.js
+++ frontend/src/components/itemListings/itemListings.js
@@ -6,13 +6,14 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import "../../assets/css/itemListings.scss";
 
 import axios from 'axios';
 
 let currTable = []
 
-function makeItemRow(itemId, name, price, description, status, currBid, maxBid, minBid) {
-    return { itemId, name, price, description, status, currBid, maxBid, minBid };
+function makeItemRow(itemId, name, price, description, status, curBid, maxBid, minBid) {
+    return { itemId, name, price, description, status, curBid, maxBid, minBid };
 }
 
 axios({
@@ -34,22 +35,22 @@ for (let idx = 0; idx < currTable.length; idx++) {
         currItem["price"],
         currItem["description"],
         currItem["status"],
-        currItem["currBid"],
+        currItem["curBid"],
         currItem["maxBid"],
-        currBid["minBid"]
+        currItem["minBid"]
     )
 }
 
 export default function ItemListTable() {
     return (
-        <TableContainer component={Paper}>
+        <TableContainer>
             <Table sx={{ minWidth: 650 }} aria-label="simple_table">
                 <TableHead>
                     <TableRow>
                         <TableCell>Item ID</TableCell>
                         <TableCell align="right">Image</TableCell>
-                        <TableCell align="right">Name</TableCell>
-                        <TableCell align="right">Description</TableCell>
+                        <TableCell align="right" background='$secondary'>Name</TableCell>
+                        <TableCell align="right" className="desc-col">Description</TableCell>
                         <TableCell align="right">Price</TableCell>
                         <TableCell align="right">Current Status</TableCell>
                     </TableRow>
@@ -58,14 +59,13 @@ export default function ItemListTable() {
                     {currTable.map((row) => (
                         <TableRow
                             key={row.itemId}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
                             <TableCell component="th" scope="row">
                                 {row.image}
                             </TableCell>
-                            <TableCell align="right">{row.name}</TableCell>
-                            <TableCell align="right">{row.description}</TableCell>
-                            <TableCell align="right">{row.price}</TableCell>
+                            <TableCell align="right" background='$secondary'>{row.name}</TableCell>
+                            <TableCell align="right" className="desc-col">{row.description}</TableCell>
+                            <TableCell align="right" background='$secondary'>{row.price}</TableCell>
                             <TableCell align="right">{row.status}</TableCell>
                         </TableRow>
                     ))}
