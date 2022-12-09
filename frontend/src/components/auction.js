3f89c7fb556f65afa1add9da7f4d0e450c9c98b0
srcalo <smilingcalo@gmail.com>
2022-12-09 11:53:30 -0500

    Built item info section

--- frontend/src/components/auction.js
+++ frontend/src/components/auction.js
@@ -5,12 +5,20 @@ import axios from "axios";
 import "../assets/css/auctions.scss";
 import { Grid } from "@material-ui/core";
 
+let itemInfo = {
+	name: "",
+	imgDir: "",
+	desc: "",
+};
+
 const Auction = (props) => {
 	return (
 		<div className="auction">
 			<Grid container>
 				<Grid item className="item-details">
-					<img src="" />
+					<img src={itemInfo.imgDir} />
+					<h2>Name: {itemInfo.name}</h2>
+					<p className="item-desc">{itemInfo.desc}</p>
 				</Grid>
 				<Grid item className="live-section"></Grid>
 			</Grid>
